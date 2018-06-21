f4b6c35b1cf40fa3dc8b97786200183474469537
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-06-21 13:05:14 +0100

    Attacks rendered

--- src/morph/GUI/ActorSquare.hpp
+++ src/morph/GUI/ActorSquare.hpp
@@ -2,8 +2,18 @@
 #define __GUI_ACTOR_SQUARE__
 
 #include "Actor.hpp"
+#include <SDL2/SDL.h>
 
 class GraphicActorSquare : public GraphicActor {
+	private:
+	void _renderAttacks(int displayShiftX, int displayShiftY, Actor *actor);
+	void _renderAttack(
+		const SDL_Rect r,
+		const unsigned char red,
+		const unsigned char green,
+		const unsigned char blue
+	);
+
 	public:
 	void render(
 		int displayShiftX,

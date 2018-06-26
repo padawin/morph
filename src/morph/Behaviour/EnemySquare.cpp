a87320d22b24ad10a7e536015f7bf8a44a343fde
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-06-26 19:00:50 +0100

    Basic attack damages implemented for square enemies

--- src/morph/Behaviour/EnemySquare.cpp
+++ src/morph/Behaviour/EnemySquare.cpp
@@ -1,6 +1,28 @@
+#include "SDL2/SDL.h"
 #include "EnemySquare.hpp"
+#include "Physics.hpp"
 
 bool BehaviourEnemySquare::update(Engine* engine, Actor* actor) {
+	std::shared_ptr<Actor> player = engine->getHero();
+	bool playerIsHit = false,
+		 actorIsHit = false;
+	for (SDL_Rect attack : player->getAttacks()) {
+		if (physics::areRectIntersecting(attack, actor->getHitbox())) {
+			actorIsHit = true;
+		}
+	}
+	for (SDL_Rect attack : actor->getAttacks()) {
+		if (physics::areRectIntersecting(attack, player->getHitbox())) {
+			playerIsHit = true;
+		}
+	}
+
+	if (actorIsHit) {
+		actor->setHealth(actor->getHealth() - player->getAttack());
+	}
+	else if (playerIsHit) {
+		player->setHealth(player->getHealth() - actor->getAttack());
+	}
 	// if player's attack is touching the actor
 	//		remove health
 	// else if is attacking and touching player

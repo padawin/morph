66128b56e77db79674b88cdde0bfc0df368e37e9
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-07-02 23:12:13 +0100

    health bar displayed on the right hand side

--- src/morph/HUD.cpp
+++ src/morph/HUD.cpp
@@ -2,25 +2,16 @@
 #include "Actor.hpp"
 #include "SDL2_framework/Game.h"
 
-const int HP_HEIGHT = 16;
-
 void HUD::render(Game *game, std::shared_ptr<Actor> hero) {
-	int leftMargin = 10;
-	int maxHealthWidth = game->getScreenWidth() / 4 - leftMargin;
-	int displayShiftX = leftMargin;
-	int displayShiftY = game->getScreenHeight() - 10 - HP_HEIGHT;
-	int health = 0;
-	if (hero->getHealth() > 0) {
-		health = hero->getHealth();
-	}
+	int healthWidth = 16;
 	SDL_Rect r;
-	r.x = displayShiftX;
-	r.y = displayShiftY;
-	r.w = maxHealthWidth * health / hero->getMaxHealth();
-	r.h = HP_HEIGHT;
+	r.x = game->getScreenWidth() - healthWidth;
+	r.w = healthWidth;
+	r.h = hero->getHealth() * game->getScreenHeight() / hero->getMaxHealth();
+	r.y = game->getScreenHeight() - r.h;
 	SDL_SetRenderDrawColor(game->getRenderer(), 255, 0, 0, 255);
 	SDL_RenderFillRect(game->getRenderer(), &r);
-	r.w = maxHealthWidth;
+	r.h = game->getScreenHeight();
 	SDL_SetRenderDrawColor(game->getRenderer(), 0, 0, 0, 255);
 	SDL_RenderDrawRect(game->getRenderer(), &r);
 }

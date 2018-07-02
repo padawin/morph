e4f46a04948666d260dc556a3e98a6ab15f85a2a
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-07-02 23:05:05 +0100

    if the player touches the attack of an actor, cancel the attack

--- src/morph/Behaviour/EnemySquare.cpp
+++ src/morph/Behaviour/EnemySquare.cpp
@@ -7,17 +7,26 @@ bool BehaviourEnemySquare::update(Engine* engine, Actor* actor) {
 	std::shared_ptr<Actor> player = engine->getHero();
 	bool playerIsHit = false,
 		 actorIsHit = false;
-	for (auto const& attack : player->getAttacks()) {
-		if (physics::areRectIntersecting(attack.second, actor->getHitbox())&& !actor->hasInvincibilityFrame()) {
+	for (auto const& playerAttack : player->getAttacks()) {
+		if (physics::areRectIntersecting(playerAttack.second, actor->getHitbox())&& !actor->hasInvincibilityFrame()) {
 			actorIsHit = true;
+			goto testHitPlayer;
+		}
+		for (auto const& actorAttack : actor->getAttacks()) {
+			if (physics::areRectIntersecting(playerAttack.second, actorAttack.second)) {
+				actorIsHit = true;
+				goto testHitPlayer;
+			}
 		}
 	}
 	for (auto const& attack : actor->getAttacks()) {
 		if (physics::areRectIntersecting(attack.second, player->getHitbox())) {
 			playerIsHit = true;
+			break;
 		}
 	}
 
+	testHitPlayer:
 	if (actorIsHit) {
 		actor->setHealth(actor->getHealth() - player->getAttack());
 		actor->setInvincibilityFrame();
